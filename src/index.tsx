dde5654426946f7246b7e0abf531c05209b745d9
Colgate13 <gabreilbarros13@gmail.com>
2021-08-18 10:53:44 -0300

    Configure dotenv

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react';
+import dotenv from 'dotenv';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+dotenv.config();
 
 ReactDOM.render(
   <React.StrictMode>
